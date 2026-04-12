2865b23f9999a70c31f0af31176db98dc991450a
Mariia Hoienko <mariiahoienko1999@gmail.com>
2026-04-12 15:27:38 +0200

    fix firebase config for deployment

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -2,18 +2,16 @@ import { initializeApp, getApps } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyDacAZAfo_L05z2okPdebOXe1mYVDCyyrk',
+  authDomain: 'wishlist-a64f9.firebaseapp.com',
+  projectId: 'wishlist-a64f9',
+  storageBucket: 'wishlist-a64f9.firebasestorage.app',
+  messagingSenderId: '581419988392',
+  appId: '1:581419988392:web:0c9e6d1ca929a5e039d99d',
+}
+
 export default defineNuxtPlugin(() => {
-  const config = useRuntimeConfig()
-
-  const firebaseConfig = {
-    apiKey: config.public.firebaseApiKey,
-    authDomain: config.public.firebaseAuthDomain,
-    projectId: config.public.firebaseProjectId,
-    storageBucket: config.public.firebaseStorageBucket,
-    messagingSenderId: config.public.firebaseMessagingSenderId,
-    appId: config.public.firebaseAppId,
-  }
-
   const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
   const db = getFirestore(app)
   const storage = getStorage(app)
